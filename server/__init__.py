81af00fb6c1d288450c5365fff35d3a97bf430bc
Adithya Menon R <adukottan@gmail.com>
2026-04-10 02:52:39 +0530

    ref: cleaned up client.py

--- server/__init__.py
+++ server/__init__.py
@@ -4,8 +4,6 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-"""Micro Soc Gym environment server components."""
-
 from .micro_soc_gym_environment import MicroSocGymEnvironment
 
 __all__ = ["MicroSocGymEnvironment"]
